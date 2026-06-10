85bd9f72bd7969044cf28373d16c74e7f3399185
1hundr3d <1hunnndr3d@gmail.com>
2026-06-10 09:16:13 +0000

    feat: auth and reg

---
+++ app/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.database import Base, engine
+from app.routers.auth import router as auth_router
+from contextlib import asynccontextmanager
+import logging
+
+logger = logging.getLogger(__name__)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("Сервер запускается")
+    Base.metadata.create_all(bind=engine)
+    yield
+    logger.info("Сервер выключается")
+
+app = FastAPI(title='FakeStore Backend', lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(auth_router)
