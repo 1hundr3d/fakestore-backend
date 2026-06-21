5924f394df5c6f111dcbba8e66f493006afea55d
1hundr3d <1hunnndr3d@gmail.com>
2026-06-21 08:23:43 +0000

    #

--- app/routers/products.py
+++ app/routers/products.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database import get_db
-from app.models import Products
+from app.models import Products, UserDB
 from app.schemas import ProductOut, ProductCreate
 from typing import Optional
+from app.auth import get_current_user
 
 router = APIRouter(prefix="/products", tags=["products"], redirect_slashes=False)
 
@@ -16,7 +17,7 @@ async def products(search: Optional[str] = None, db: Session=Depends(get_db)):
     return existing_search
 
 @router.post("/", response_model=ProductOut)
-async def create_product(product_data: ProductCreate, db: Session=Depends(get_db)):
+async def create_product(product_data: ProductCreate, db: Session=Depends(get_db), current_user: UserDB=Depends(get_current_user)):
     new_product = Products(title = product_data.title, price = product_data.price, description = product_data.description, image = product_data.image)
     db.add(new_product)
     db.commit()
